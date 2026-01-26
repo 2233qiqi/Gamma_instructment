8996171e377137cdb72c0c837a3b255fa8793667
2233qiqi <2233qiqi>
2026-01-26 18:38:06 +0800

    1/26

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -2,27 +2,31 @@
 #include "G4Run.hh"
 #include "G4SystemOfUnits.hh"
 
-RunAction :: RunAction() :G4UserRunAction()
+RunAction::RunAction() : G4UserRunAction()
 {
-    auto analysisManager = G4AnalysisManager ::Instance();
-    analysisManager->SetDefaultFileType("root");
-    analysisManager->SetFileName("output.root");
+    auto analysisManager = G4AnalysisManager::Instance();
+    
 
+    analysisManager->SetDefaultFileType("root");
+    analysisManager->SetFileName("output"); 
+    analysisManager->CreateH1("E_dep", "Energy Deposition", 3000, 0., 3000.*keV);
 }
+
 RunAction::~RunAction()
 {
 }
 
 void RunAction::BeginOfRunAction(const G4Run*)
 {
-  auto analysisManager = G4AnalysisManager::Instance();
-  analysisManager->OpenFile();
+    auto analysisManager = G4AnalysisManager::Instance();
+    analysisManager->OpenFile();
 }
 
 void RunAction::EndOfRunAction(const G4Run*)
 {
-  auto analysisManager = G4AnalysisManager::Instance();
-
-  analysisManager->Write();
-  analysisManager->CloseFile();
+    auto analysisManager = G4AnalysisManager::Instance();
+    
+    // 写入数据并关闭
+    analysisManager->Write();
+    analysisManager->CloseFile();
 }
