882ef5871d2925d11a5b4f9b8df38e411963215b
2233qiqi <1091682015@qq.com>
2026-03-28 22:06:07 +0800

    3/28

--- main.cc
+++ main.cc
@@ -22,7 +22,7 @@ int main(int argc, char** argv)
 
     auto* UImanager = G4UImanager::GetUIpointer();
 
-    UImanager->ApplyCommand("/control/execute vis.mac");
+    UImanager->ApplyCommand("/control/execute run.mac");
 
     ui->SessionStart();
 
